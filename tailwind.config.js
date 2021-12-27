d56fe1be25c0ef63939be56ccbe837ad47b10c51
Sumon Chandra Shil <topu199041@gmail.com>
2021-12-27 13:37:33 +0600

    added Header section

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {
+      display: ['group-focus'],
+      opacity: ['group-focus'],
+      inset: ['group-focus']
+    }
+  },
+  plugins: [],
+}
